0a446bb3413e4150b4a949d60633de5ffefa300c
lawrencewong <lawrencecywong@gmail.com>
2014-09-19 15:19:01 -0400

    Just styling to do

--- helloworld/helloworld/controllers/users.py
+++ helloworld/helloworld/controllers/users.py
@@ -8,6 +8,7 @@ from helloworld.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
 
+#Hardcoded data to simulate a databse of users
 user1 = {'id': '1', 'firstname' : 'Lawrence', 'lastname' : 'Wong', 'role' : 'Developer'}
 user2 = {'id': '2', 'firstname' : 'Greg', 'lastname' : 'Klotz', 'role' : 'Professor'}
 user3 = {'id': '3', 'firstname' : 'Links', 'lastname' : 'Cat', 'role' : 'Feline'}    
@@ -15,40 +16,42 @@ data = {
 	  '1' : user1,
 	  '2' : user2,
 	  '3' : user3
-	}
-userCount = 3;
+}
+userCount = 3
+
 class UsersController(BaseController):
     
-    def addUser(self):
-    	if request.method == 'POST':
-    		global userCount
-    		global data
-    		userCount+=1
-    		newUser = {'id' : userCount, 'firstname' : request.POST.get('firstName'), 'lastname' : request.POST.get('lastName'), 'role' : 'Guest'}
-    		data[userCount] = newUser
-    		return json.dumps(newUser)
-	  
-    def changeName(self, userid):
-      c.userid = userid
-      if request.method == 'PUT':
-		if userid in data:
-			data[userid]['firstname'] = request.POST.get('firstName')
-			data[userid]['lastname'] = request.POST.get('lastName')
-			return json.dumps(data[userid]) 
-		else:
-			return json.dumps({'error':'User not found.'})
-      
-    def userCheck(self, userid):
-      c.userid = userid
-      global data
-      if request.method == 'GET':
-		if userid in data:
-		  return json.dumps(data[userid])
-		else:
- 		  return json.dumps({'error':'Cannot check for user. User ID not found.'})
+    #addUser function adds to the dictionary of users incrementing the userCount for 'unique' ID's. Utilizing POST request.
+	def addUser(self):
+		if request.method == 'POST':
+			global userCount
+			userCount+=1
+			newUser = {'id' : userCount, 'firstname' : request.POST.get('firstName'), 'lastname' : request.POST.get('lastName'), 'role' : 'Guest'}
+			data[str(userCount)] = newUser
+			return json.dumps(newUser)
 
-      
-    def removeUser(self, userid):
+	#changeName changes the first and last name of a give user ID. Using PUT requests.
+	def changeName(self, userid):
+		c.userid = userid
+		if request.method == 'PUT':
+			if userid in data:
+				data[userid]['firstname'] = request.POST.get('firstName')
+				data[userid]['lastname'] = request.POST.get('lastName')
+				return json.dumps(data[userid]) 
+			else:
+				return json.dumps({'error':'User not found.'})
+
+	#userCheck function to look up a given user based on user ID. Using GET requests.
+	def userCheck(self, userid):
+		c.userid = userid
+		if request.method == 'GET':
+			if userid in data:
+				return json.dumps(data[userid])
+			else:
+				return json.dumps({'error':'Cannot check for user. User ID not found.'})
+
+	#remoceUser deletes a user from the 'database' based on user ID. Using DELETE requests.
+	def removeUser(self, userid):
 		c.userid = userid
 		if request.method == 'DELETE':
 			if userid in data:
