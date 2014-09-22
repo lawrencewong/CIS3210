ad0d80fce7d211584222c36cc5ef7314e48cdff4
Lawrence Wong <lwong01@mac-9773.socs.uoguelph.ca>
2014-09-22 17:30:50 -0400

    Selenium test case done for frontend, user controller tests have been set up

--- helloworld/helloworld/tests/functional/test_users.py
+++ helloworld/helloworld/tests/functional/test_users.py
@@ -3,5 +3,9 @@ from helloworld.tests import *
 class TestUsersController(TestController):
 
     def test_index(self):
-        response = self.app.get(url(controller='users', action='index'))
-        # Test response...
+	response = self.app.get(url(controller='users', action='userCheck', userid='1'))\
+	print response
+	assert '"id": "1"' in response
+	assert '"firstname" : "Lawrence"' in response
+	assert '"lastname" : "Wong"' in response
+	assert '"role" : "Developer"' in response
